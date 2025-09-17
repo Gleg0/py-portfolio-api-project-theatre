a0b6918f75a0a65aa561eb5d8c35bf690bdc866c
Glego <glego200323@gmail.com>
2025-09-18 02:04:24 +0300

    Small fixes for Docker.

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,7 +10,7 @@ echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
 echo "Starting Gunicorn..."
-exec gunicorn cinema_service.wsgi:application \
+exec gunicorn config.wsgi:application \
     --bind 0.0.0.0:8000 \
     --workers 3 \
     --timeout 120
